11982b7fc3fdbe54a0905700145e429338555f03
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-05-23 19:17:32 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,18 +1,29 @@
-def index_of_by_index(word, list, index):
+def index_of_by_index(word, lst, start_index):
+    for index, element in enumerate(lst[start_index:], start_index):
+        if element == word:
+            return index
+    return -1
+def index_of_empty(lst):
+    for index, element in enumerate(lst):
+        if element == "":
+            return index
+    return -1
+def index_of(word, lst):
+    for index, element in enumerate(lst):
+        if element == word:
+            return index
+    return -1
+def put(word, lst):
+    for index, element in enumerate(lst):
+        if element == "":
+            lst[index] = word
+            return index
     return -1
 
-
-def index_of_empty(list):
-    return -1
-
-
-def index_of(word, list):
-    return -1
-
-
-def put(word, list):
-    return -1
-
-
-def remove(word, list):
-    return -1
+def remove(word, lst):
+    count = 0
+    for index, element in enumerate(lst):
+        if element == word:
+            lst[index] = ""
+            count += 1
+    return count
