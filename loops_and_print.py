2b8b3049a1d74c5e6bd2fde2bc59c8c17ab5a1f4
papiyon8989 <162721612+papiyon8989@users.noreply.github.com>
2024-05-23 19:18:11 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,17 @@
-def enumerate_list(list):
-    return list
-
-
-def enumerate_backwards(list):
-    return list
+def enumerate_list(strings):
+    result = []
+    count = 0 
+    for string in strings:
+        if string: 
+            result.append(f"{count}. {string}")
+            count += 1
+    return result
+def enumerate_backwards(strings):
+    result = []
+    count = 0  
+    for string in strings:
+        if string: 
+            reversed_string = string[::-1]
+            result.append(f"{count}. {reversed_string}")
+            count += 1
+    return result
